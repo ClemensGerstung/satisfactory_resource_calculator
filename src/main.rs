590f75b506c5e641246579af7f8cb7976b5bb0b2
Clemens Gerstung <clemens8496@gmail.com>
2021-12-16 21:13:39 +0100

    more implementation (not working)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
+use std::ops::Index;
 
 pub struct Resource {
   quantity: f64,
@@ -12,11 +13,84 @@ pub struct Recipe {
   output: Vec<Resource>,
 }
 
+pub struct Recipes<'a> {
+  recipes: Vec<Recipe>,
+  output_references: HashMap<&'a Resource, Vec<&'a Recipe>>,
+}
+
+impl Recipes<'_> {
+  pub fn new<'a>() -> Recipes<'a> {
+    return Recipes {
+      recipes: vec![],
+      output_references: HashMap::new()
+    }
+  }
+
+  pub fn add_recipe(&'static mut self,
+                    input: Vec<Resource>,
+                    output: Vec<Resource>) -> &Recipe {
+
+    self.recipes.push(Recipe { input, output });
+    let recipe = self.recipes.index(self.recipes.len() - 1);
+
+    for o in recipe.output.iter() {
+      self.output_references.entry(&o).or_insert(vec![recipe.borrow()]);
+    }
+
+    return recipe;
+  }
+
+  pub fn calculate(&self, target: &Resource, rate: f64) -> Vec<&Resource> {
+    let mut resources: Vec<&Resource> = vec![];
+
+    let recipe = self.first_recipe(target);
+
+    for input in recipe.input.iter() {
+      let base_recipe = self.first_recipe(input);
+      let found = match base_recipe.output.iter().find(|r| r.name == input.name)
+      {
+        Some(r) => r,
+        None => continue
+      };
+
+      let input_rate = (rate * input.quantity) / found.quantity;
+      let mut input_input = self.calculate(input, input_rate);
+      input_input.push(input);
+
+      for r in input_input {
+        match resources.iter_mut().find(|f| f.name == r.name) {
+          Some(d) => {
+            d.quantity += r.quantity;
+          },
+          None => resources.push(r)
+        };
+      }
+    }
+
+    return resources;
+  }
+
+  fn first_recipe(&self, resource: &Resource) -> &Recipe {
+    let elem = match self.output_references.get(resource) {
+      Some(resource) => resource,
+      None => panic!()
+    };
+
+    let recipe = match elem.first() {
+      Some(r) => *r,
+      None => panic!()
+    };
+
+    return recipe;
+  }
+}
+
 impl PartialEq for Resource {
   fn eq(&self, other: &Self) -> bool {
     self.name == other.name
   }
 }
+
 impl Eq for Resource {}
 
 impl Hash for Resource {
@@ -37,36 +111,22 @@ impl Recipe {
              output: Vec<Resource>) -> Recipe {
     return Recipe {
       input,
-      output
+      output,
     };
-  }
-
-  pub fn calculate(&self, rate: f64) -> Recipe {
-    let mut copy = Recipe::default();
-
-    for i in self.input.iter() {
-      // copy.input.push(Resource {
-      //   name: i.name.clone(),
-      //   quantity: (i.quantity * rate),
-      // });
-    }
-
-    return copy;
   }
 }
 
 fn main() {
-  let mut r1 = Resource
+  let r1 = Resource
   {
     name: String::from("Hello"),
     quantity: 1.0,
   };
 
-  let recipe = Recipe::new(vec![r1], vec![]);
+  let r2 = Resource
+  {
+    name: String::from("Hello"),
+    quantity: 1.0,
+  };
 
-  let mut recipes: HashMap<&Resource, Vec<&Recipe>> = HashMap::new();
-  recipes.insert(&recipe.input[0], vec![&recipe]);
-
-  let mut rs = recipes.entry(&recipe.input[0]).or_insert(vec![]);
-  rs.push(&recipe);
 }
